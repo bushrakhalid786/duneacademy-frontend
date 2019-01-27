8c705274b7a9bb4239ecb2bb063ab867a8f6a8b6
Nikitha Rokhade <nikitha.rokhade@accionlabs.com>
2019-01-27 21:49:26 +0530

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login-module/login/login.component';
+import { RegisterComponent } from './login-module/register/register.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+
+
+
+const routes: Routes = [
+    {
+        path: '',
+        component: LoginComponent
+    },
+    {
+        path: 'register',
+        component: RegisterComponent,
+        pathMatch: 'full'
+    },
+    {
+        path: 'dashboard',
+        component: AdminLayoutComponent,
+        children: [
+            {
+                path: '',
+                loadChildren: './layouts/admin-layout/admin-layout.module#AdminLayoutModule'
+            }
+        ]
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './login-module/login/login.component';
+import { RegisterComponent } from './login-module/register/register.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+import {
+    MatButtonModule,
+    MatInputModule,
+    MatRippleModule,
+    MatFormFieldModule,
+    MatTooltipModule,
+    MatSelectModule
+} from '@angular/material';
+import { ComponentsModule } from './components/components.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        LoginComponent,
+        RegisterComponent,
+        AdminLayoutComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ComponentsModule,
+        BrowserAnimationsModule,
+        LayoutModule,
+        MatButtonModule,
+        MatRippleModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatSelectModule,
+        MatTooltipModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
